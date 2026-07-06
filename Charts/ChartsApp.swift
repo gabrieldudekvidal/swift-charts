b7603e84480592f14f676ea8de7bea6d93c16e30
Gabriel Vidal <gabrieldkvl@icloud.com>
2026-07-06 16:11:59 +0800

    Initial Commit

---
+++ Charts/ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsApp.swift
+//  Charts
+//
+//  Created by Gabriel Vidal on 2026-07-06.
+//
+
+import SwiftUI
+
+@main
+struct ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
